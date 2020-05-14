3e3d29c0d448ff2e6a3e5c89178759f57765fa84
Yujiahao <myccy123@126.com>
2020-05-14 18:22:48 +0800

    样式调整

--- src/components/home/imax.vue
+++ src/components/home/imax.vue
@@ -2,7 +2,7 @@
     <div id="example" style="position: relative;">
         <img :src="bgImg" style="height: 100%;width: 100%;object-fit: cover">
         <div class="imax">
-            <h1 style="font-size: 63px;font-weight: 500;color: #303133;text-align: center">一分钟数据可视化</h1>
+            <h1 style="margin: 3% 0 1% 0; font-size: 63px;font-weight: 500;color: #303133;text-align: center">一分钟数据可视化</h1>
             <p style="font-size: 21px;font-weight: 200;text-align: center">灵活的数据源管理，丰富的图表库，酷炫的驾驶舱大屏</p>
             <p style="font-size: 21px;font-weight: 200;text-align: center">便捷集成，随心定制</p>
         </div>
