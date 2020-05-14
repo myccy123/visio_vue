31edfcf3cc75687fac22f3d53dff116bad938f37
renxionghui <renxionghui@126.com>
2020-05-14 17:43:49 +0800

    feat:增加注册页面

--- src/components/signin/Signup.vue
+++ src/components/signin/Signup.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="signin-wrap">
+    <div class="signup-wrap">
         <div style='position:absolute;top:24px;left:24px'>
             <el-button style='font-size:24px' type="primary" icon="el-icon-s-home" circle @click="$router.replace('/')"></el-button>
         </div>
-        <div class="signin-input-wrap">
+        <div class="signup-input-wrap">
             <el-form :model="signModel" :rules="signRules" ref="ruleForm">
                 <el-form-item prop="user">
                     <el-input
@@ -23,33 +23,43 @@
                         @keyup.enter.native="handleSubmit"
                     ></el-input>
                 </el-form-item>
+                <el-form-item prop="verify">
+                    <el-input
+                        clearable
+                        v-model="signModel.verify"
+                        show-password
+                        type="password"
+                        placeholder="请确认密码"
+                        @keyup.enter.native="handleSubmit"
+                    ></el-input>
+                </el-form-item>
                 <el-form-item>
-                    <el-button type="primary" style="width:100%" @click="handleSubmit">登录</el-button>
+                    <el-button type="primary" style="width:100%" @click="handleSubmit">注册</el-button>
                 </el-form-item>
             </el-form>
-            <div style="display:flex;justify-content:space-between">
-                <el-checkbox :disabled="isSignup" label="记住密码" v-model="isRememberPwd"></el-checkbox>
-                <span class="btn-to-signup" @click="$router.push({ name: 'signup' })">注册</span>
-            </div>
         </div>
     </div>
 </template>
 <script>
-const KEY_USER = "user",
-    KEY_PASSWORD = "password",
-    KEY_REMEMBER = "isRemember",
-    KEY_SIGNIN = "isSignin",
-    KEY_INTERCEPT = "routerIntercept";
 export default {
-    name: "Signin",
+    name: "Signup",
     data() {
+        //二次校验
+        const validator = (rule, value, callback) => {
+            if (value === "") {
+                callback(new Error("请再次输入密码"));
+            } else if (value !== this.signModel.password) {
+                callback(new Error("两次输入密码不一致!"));
+            } else {
+                callback();
+            }
+        };
         return {
             signModel: {
                 user: "",
-                password: ""
+                password: "",
+                verify: ""
             },
-            isRememberPwd: false,
-            isSignup: false,
             signRules: {
                 user: [
                     { required: true, message: "请输入用户名" },
@@ -65,78 +75,49 @@ export default {
                         message:
                             "最少6位，包括至少1个大写字母，1个小写字母，1个数字"
                     }
+                ],
+                verify:[
+                    { required: true,message: "请确认密码" },
+                    { validator: validator, trigger: 'blur' }
                 ]
             }
         };
-    },
-    watch: {
-        isRememberPwd(val) {
-            this.setLsItem(KEY_REMEMBER, val);
-        }
-    },
-    mounted() {
-        this.isRememberPwd = this.getLsItem(KEY_REMEMBER);
-        this.signModel.user = this.getLsItem(KEY_USER);
-        this.signModel.password = this.getLsItem(KEY_PASSWORD);
     },
     methods: {
         handleSubmit() {
             this.$refs.ruleForm.validate(valid => {
                 if (valid) {
-                    this.signin();
+                    this.signup();
                 } else {
                     return false;
                 }
             });
         },
-        //登录
-        signin() {
+        //注册
+        signup() {
             const { user, password } = this.signModel;
             let params = {
                 userid: user,
                 passWord: password
             };
             this.$axios
-                .post(this.$api.authSignin, params)
+                .post(this.$api.authSignup, params)
                 .then(res => {
-                    console.log(res);
                     const resData = res.data;
                     if (res.status === 200 && resData.code === "00") {
-                        let lsUser = "";
-                        let lsPassword = "";
-                        //如果勾选记住密码
-                        if (this.getLsItem(KEY_REMEMBER)) {
-                            let lsUser = params.user;
-                            let lsPassword = params.password;
-                        }
-                        this.setLsItem(KEY_USER, lsUser);
-                        this.setLsItem(KEY_PASSWORD, lsPassword);
-
-                        //保存登录状态
-                        this.setSsItem(KEY_SIGNIN, true);
-
-                        //跳转到登录前的页面
-                        let fullPath = this.getSsItem(KEY_INTERCEPT) || "/";
-                        this.$router.replace({ path: fullPath });
-                        //保存登录状态
-                        this.$store.commit("setIsSignin", true);
-                        //保存用户信息
-                        this.$store.commit("setUserInfo", resData.data);
+                        this.$message.success("注册成功");
+                        this.$router.replace({name:'signin'})
                     } else {
                         this.$message.error(resData.message);
-                        this.$store.commit("setIsSignin", false);
                     }
                 })
-                .catch(err => {
-                    this.$store.commit("setIsSignin", false);
-                    console.log(err);
-                });
+                .catch(err => console.log(err));
         }
     }
 };
 </script>
 <style scoped>
-.signin-wrap {
+.signup-wrap {
     width: 100vw;
     height: 100vh;
     display: flex;
@@ -144,20 +125,20 @@ export default {
     align-items: center;
 }
 
-.signin-input-wrap {
+.signup-input-wrap {
     width: 320px;
     padding: 24px;
     border-radius: 12px;
     border: 1px solid #eee;
 }
 
-.btn-to-signup {
+.signup-btn-forget {
     cursor: pointer;
     font-size: 14px;
     font-weight: bold;
 }
 
-.btn-to-signup:hover {
+.signup-btn-forget:hover {
     color: #409eff;
 }
 </style>
