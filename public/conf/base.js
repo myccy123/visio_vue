ac75a67a9fd3c6fa83d896ef9d8fe06601b6e9cb
Yujiahao <myccy123@126.com>
2020-05-21 16:17:22 +0800

    样式调整

--- public/conf/base.js
+++ public/conf/base.js
@@ -1,5 +1,12 @@
 function getBaseUrl() {
-    return 'http://192.168.43.180:8000'
+    let reg = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/;
+    let locationIp = reg.exec(window.location.href)[0];
+    //如果没有配置ip地址则从浏览器输入的地址中获取ip地址
+    let IC_VUE_IP = '';
+    if(!IC_VUE_IP){
+        IC_VUE_IP = locationIp;
+    }
+    return 'http://' + IC_VUE_IP + ':9000'
 }
 
 function getSliderInterval(){
