3f6573d68ad2413d0997161cb8eaf98cc0fcf2db
yujiahao <932320908@qq.com>
2020-10-08 20:52:23 +0800

    解决表头重置的BUG

--- public/conf/base.js
+++ public/conf/base.js
@@ -5,8 +5,8 @@ function getHrefIp() {
 }
 
 function getBaseUrl() {
-    let baseUrl = 'http://127.0.0.1:8000';
-    // let baseUrl = 'http://183.251.100.172:9000';
+    // let baseUrl = 'http://127.0.0.1:8000';
+    let baseUrl = 'http://183.251.100.172:9000';
     // 如果没有配置ip地址则从浏览器输入的地址中获取ip地址
     if(!baseUrl){
         return getHrefIp()
